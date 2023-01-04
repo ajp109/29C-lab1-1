f43211fb1d0d9638d7cb655badcb9d82cc5b9af0
ajp109 <andrew.pomfret@york.ac.uk>
2023-01-04 15:07:15 +0000

    Initial commit of project-specific code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include "mbed.h"
 
+int x;
+
 int main() {
+
+    int a;
+
+    printf("x is stored at address %p\r\n", &x);
+    printf("a is stored at address %p\r\n", &a);
+
     while (true) {
         sleep();
     }
